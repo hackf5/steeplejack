63e878fd27d4ac3e682fffed84e9d7625950f9cb
Brian Tyler <brian@hackf5.io>
2025-11-20 23:46:45 +0000

    refactor: extract pipeline creation and descriptor set fetching into standalone functions

--- src/vulkan/shadow_pipeline.cpp
+++ src/vulkan/shadow_pipeline.cpp
@@ -7,36 +7,19 @@
 
 using namespace steeplejack;
 
-ShadowPipeline::ShadowPipeline(
+namespace
+{
+[[nodiscard]] VkPipeline create_pipeline(
     const Device& device,
-    DescriptorSetLayout& descriptor_set_layout,
     const ShadowRenderPass& shadow_render_pass,
+    VkPipelineLayout pipeline_layout,
     std::string_view vertex_shader,
-    std::string_view fragment_shader) :
-    m_device(device),
-    m_descriptor_set_layout(descriptor_set_layout),
-    m_pipeline_layout(m_descriptor_set_layout.create_pipeline_layout()),
-    m_pipeline(create_pipeline(shadow_render_pass, vertex_shader, fragment_shader)),
-    vkCmdPushDescriptorSetKHR(fetch_vkCmdPushDescriptorSetKHR())
+    std::string_view fragment_shader)
 {
-}
-
-ShadowPipeline::~ShadowPipeline()
-{
-    spdlog::info("Destroying Shadow Pipeline");
-    vkDestroyPipeline(m_device.vk(), m_pipeline, nullptr);
-    vkDestroyPipelineLayout(m_device.vk(), m_pipeline_layout, nullptr);
-}
-
-VkPipeline ShadowPipeline::create_pipeline(
-    const ShadowRenderPass& shadow_render_pass, std::string_view vertex_shader, std::string_view fragment_shader) const
-{
-    spdlog::info("Creating Shadow Pipeline");
-
     auto vertex_input_state = VertexInputState::make_position_only(0);
 
-    auto vertex_shader_module = ShaderModule(m_device, vertex_shader);
-    auto fragment_shader_module = ShaderModule(m_device, fragment_shader);
+    auto vertex_shader_module = ShaderModule(device, vertex_shader);
+    auto fragment_shader_module = ShaderModule(device, fragment_shader);
     auto shader_stages = pipeline::create_shader_stages(vertex_shader_module, fragment_shader_module);
 
     auto input_assembly_state = pipeline::create_input_assembly_state();
@@ -66,13 +49,13 @@ VkPipeline ShadowPipeline::create_pipeline(
     pipeline_info.pColorBlendState = nullptr;
     pipeline_info.pDynamicState = &dynamic_state;
     pipeline_info.pDepthStencilState = &depth_stencil_state;
-    pipeline_info.layout = m_pipeline_layout;
+    pipeline_info.layout = pipeline_layout;
     pipeline_info.renderPass = shadow_render_pass.vk();
     pipeline_info.subpass = 0;
     pipeline_info.basePipelineHandle = nullptr;
 
     VkPipeline pipeline = nullptr;
-    if (vkCreateGraphicsPipelines(m_device.vk(), nullptr, 1, &pipeline_info, nullptr, &pipeline) != VK_SUCCESS)
+    if (vkCreateGraphicsPipelines(device.vk(), nullptr, 1, &pipeline_info, nullptr, &pipeline) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to create pipeline");
     }
@@ -80,14 +63,59 @@ VkPipeline ShadowPipeline::create_pipeline(
     return pipeline;
 }
 
-PFN_vkCmdPushDescriptorSetKHR ShadowPipeline::fetch_vkCmdPushDescriptorSetKHR()
+[[nodiscard]] PFN_vkCmdPushDescriptorSetKHR fetch_vkCmdPushDescriptorSetKHR(const Device& device) // NOLINT(readability-identifier-naming)
 {
-    auto result = reinterpret_cast<PFN_vkCmdPushDescriptorSetKHR>(
-        vkGetDeviceProcAddr(m_device.vk(), "vkCmdPushDescriptorSetKHR"));
+    auto result =
+        reinterpret_cast<PFN_vkCmdPushDescriptorSetKHR>(vkGetDeviceProcAddr(device.vk(), "vkCmdPushDescriptorSetKHR"));
     if (result == nullptr)
     {
         throw std::runtime_error("Failed to load vkCmdPushDescriptorSetKHR");
     }
 
     return result;
+}
+} // namespace
+
+ShadowPipeline::ShadowPipeline(
+    const Device& device,
+    DescriptorSetLayout& descriptor_set_layout,
+    const ShadowRenderPass& shadow_render_pass,
+    std::string_view vertex_shader,
+    std::string_view fragment_shader) :
+    m_device(device),
+    m_descriptor_set_layout(descriptor_set_layout),
+    m_pipeline_layout(m_descriptor_set_layout.create_pipeline_layout()),
+    m_pipeline(create_pipeline(device, shadow_render_pass, m_pipeline_layout, vertex_shader, fragment_shader)),
+    vkCmdPushDescriptorSetKHR(fetch_vkCmdPushDescriptorSetKHR(device))
+{
+    spdlog::info("Creating Shadow Pipeline");
+}
+
+ShadowPipeline::~ShadowPipeline()
+{
+    spdlog::info("Destroying Shadow Pipeline");
+    vkDestroyPipeline(m_device.vk(), m_pipeline, nullptr);
+    vkDestroyPipelineLayout(m_device.vk(), m_pipeline_layout, nullptr);
+}
+
+DescriptorSetLayout& ShadowPipeline::descriptor_set_layout() const
+{
+    return m_descriptor_set_layout;
+}
+
+void ShadowPipeline::bind(VkCommandBuffer command_buffer) const
+{
+    vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
+}
+
+void ShadowPipeline::push_descriptor_set(VkCommandBuffer command_buffer) const
+{
+    auto write_descriptor_sets = m_descriptor_set_layout.get_write_descriptor_sets();
+    vkCmdPushDescriptorSetKHR(
+        command_buffer,
+        VK_PIPELINE_BIND_POINT_GRAPHICS,
+        m_pipeline_layout,
+        0,
+        write_descriptor_sets.size(),
+        write_descriptor_sets.data());
 }
--- src/vulkan/shadow_pipeline.h
+++ src/vulkan/shadow_pipeline.h
@@ -19,14 +19,6 @@ class ShadowPipeline
 
     PFN_vkCmdPushDescriptorSetKHR vkCmdPushDescriptorSetKHR; // NOLINT(readability-identifier-naming)
 
-    [[nodiscard]] VkPipeline create_pipeline(
-        const ShadowRenderPass& shadow_render_pass,
-        std::string_view vertex_shader,
-        std::string_view fragment_shader) const;
-
-    [[nodiscard]] PFN_vkCmdPushDescriptorSetKHR
-    fetch_vkCmdPushDescriptorSetKHR(); // NOLINT(readability-identifier-naming)
-
   public:
     ShadowPipeline(
         const Device& device,
@@ -41,26 +33,10 @@ class ShadowPipeline
     ShadowPipeline(ShadowPipeline&&) = delete;
     ShadowPipeline& operator=(ShadowPipeline&&) = delete;
 
-    [[nodiscard]] DescriptorSetLayout& descriptor_set_layout() const
-    {
-        return m_descriptor_set_layout;
-    }
+    [[nodiscard]] DescriptorSetLayout& descriptor_set_layout() const;
 
-    void bind(VkCommandBuffer command_buffer) const
-    {
-        vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
-    }
+    void bind(VkCommandBuffer command_buffer) const;
 
-    void push_descriptor_set(VkCommandBuffer command_buffer) const
-    {
-        auto write_descriptor_sets = m_descriptor_set_layout.get_write_descriptor_sets();
-        vkCmdPushDescriptorSetKHR(
-            command_buffer,
-            VK_PIPELINE_BIND_POINT_GRAPHICS,
-            m_pipeline_layout,
-            0,
-            write_descriptor_sets.size(),
-            write_descriptor_sets.data());
-    }
+    void push_descriptor_set(VkCommandBuffer command_buffer) const;
 };
 } // namespace steeplejack
