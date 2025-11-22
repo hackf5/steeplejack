49c01916e791901ea34e133ec4c1291f288de838
Brian Tyler <brian@hackf5.io>
2025-11-22 11:13:24 +0000

    refactor: update GraphicsBuffers to use std::span for vertex and index loading

--- src/vulkan/graphics_buffers.cpp
+++ src/vulkan/graphics_buffers.cpp
@@ -11,6 +11,26 @@ GraphicsBuffers::GraphicsBuffers(const Device& device, const AdhocQueues& adhoc_
 {
 }
 
+void GraphicsBuffers::load_vertexes(std::span<const Vertex> vertexes)
+{
+    m_vertex_buffer = std::make_unique<BufferGPU>(
+        m_device,
+        m_adhoc_queues,
+        total_bytes(vertexes),
+        VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
+    m_vertex_buffer->copy_from(vertexes);
+}
+
+void GraphicsBuffers::load_indexes(std::span<const Vertex::Index> indexes)
+{
+    m_index_buffer = std::make_unique<BufferGPU>(
+        m_device,
+        m_adhoc_queues,
+        total_bytes(indexes),
+        VK_BUFFER_USAGE_INDEX_BUFFER_BIT);
+    m_index_buffer->copy_from(indexes);
+}
+
 void GraphicsBuffers::bind(VkCommandBuffer command_buffer) const
 {
     vkCmdBindVertexBuffers(command_buffer, 0, 1, m_vertex_buffer->vk_ptr(), kVertexOffsets.data());
--- src/vulkan/graphics_buffers.h
+++ src/vulkan/graphics_buffers.h
@@ -3,13 +3,10 @@
 #include "adhoc_queues.h"
 #include "buffer/buffer_gpu.h"
 #include "device.h"
-#include "util/memory.h"
 #include "vertex.h"
 
-#include <array>
 #include <memory>
-#include <ranges>
-#include <type_traits>
+#include <span>
 #include <vulkan/vulkan.h>
 
 namespace steeplejack
@@ -32,41 +29,9 @@ class GraphicsBuffers
     GraphicsBuffers& operator=(GraphicsBuffers&&) = delete;
     ~GraphicsBuffers() = default;
 
-    template <typename TIter> void load_vertexes(TIter begin, TIter end)
-    {
-        static_assert(std::is_same_v<Vertex, std::decay_t<decltype(*begin)>>, "TIter must be an iterator to Vertex");
+    void load_vertexes(std::span<const Vertex> vertexes);
 
-        m_vertex_buffer = std::make_unique<BufferGPU>(
-            m_device,
-            m_adhoc_queues,
-            total_bytes(begin, end),
-            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT);
-        m_vertex_buffer->copy_from(begin, end);
-    }
-
-    void load_vertexes(const std::ranges::contiguous_range auto& vertexes)
-    {
-        load_vertexes(std::begin(vertexes), std::end(vertexes));
-    }
-
-    template <typename TIter> void load_indexes(TIter begin, TIter end)
-    {
-        static_assert(
-            std::is_same_v<Vertex::Index, std::decay_t<decltype(*begin)>>,
-            "TIter must be an iterator to Vertex::Index");
-
-        m_index_buffer = std::make_unique<BufferGPU>(
-            m_device,
-            m_adhoc_queues,
-            total_bytes(begin, end),
-            VK_BUFFER_USAGE_INDEX_BUFFER_BIT);
-        m_index_buffer->copy_from(begin, end);
-    }
-
-    void load_indexes(const std::ranges::contiguous_range auto& indexes)
-    {
-        load_indexes(std::begin(indexes), std::end(indexes));
-    }
+    void load_indexes(std::span<const Vertex::Index> indexes);
 
     void bind(VkCommandBuffer command_buffer) const;
 };
