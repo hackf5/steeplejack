c962df392b820adaae02711f67453d4fd516518e
Brian Tyler <brian@hackf5.io>
2025-11-22 00:17:53 +0000

    refactor: move create_image_view function to a separate namespace and update its parameters

--- src/vulkan/image_view.cpp
+++ src/vulkan/image_view.cpp
@@ -6,36 +6,26 @@
 
 using namespace steeplejack;
 
-ImageView::ImageView(
-    const Device& device, const Image& image, VkImageAspectFlags aspect_mask, std::optional<uint32_t> base_layer) :
-    m_device(device), m_image(image), m_image_view(create_image_view(aspect_mask, base_layer))
+namespace
 {
-}
-
-ImageView::~ImageView()
-{
-    spdlog::info("Destroying Image View");
-
-    vkDestroyImageView(m_device.vk(), m_image_view, nullptr);
-}
-
-VkImageView ImageView::create_image_view(VkImageAspectFlags aspect_mask, std::optional<uint32_t> base_layer)
+[[nodiscard]] VkImageView create_image_view(
+    const Device& device, const Image& image, VkImageAspectFlags aspect_mask, std::optional<uint32_t> base_layer)
 {
     spdlog::info("Creating Image View");
 
-    auto array_layers = m_image.image_info().array_layers;
+    auto array_layers = image.image_info().array_layers;
 
     // base_layer can only be specified for array images
     assert(!base_layer.has_value() || (array_layers > 1 && base_layer.value() < array_layers));
 
     VkImageViewCreateInfo image_view_info = {};
     image_view_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-    image_view_info.image = m_image.vk();
+    image_view_info.image = image.vk();
 
     image_view_info.viewType =
         !base_layer.has_value() && array_layers > 1 ? VK_IMAGE_VIEW_TYPE_2D_ARRAY : VK_IMAGE_VIEW_TYPE_2D;
 
-    image_view_info.format = m_image.image_info().format;
+    image_view_info.format = image.image_info().format;
     image_view_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
     image_view_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
     image_view_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
@@ -47,10 +37,29 @@ VkImageView ImageView::create_image_view(VkImageAspectFlags aspect_mask, std::op
     image_view_info.subresourceRange.layerCount = base_layer.has_value() ? 1 : array_layers;
 
     VkImageView image_view = nullptr;
-    if (vkCreateImageView(m_device.vk(), &image_view_info, nullptr, &image_view) != VK_SUCCESS)
+    if (vkCreateImageView(device.vk(), &image_view_info, nullptr, &image_view) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to create image view");
     }
 
     return image_view;
+}
+} // namespace
+
+ImageView::ImageView(
+    const Device& device, const Image& image, VkImageAspectFlags aspect_mask, std::optional<uint32_t> base_layer) :
+    m_device(device), m_image(image), m_image_view(create_image_view(device, image, aspect_mask, base_layer))
+{
+}
+
+ImageView::~ImageView()
+{
+    spdlog::info("Destroying Image View");
+
+    vkDestroyImageView(m_device.vk(), m_image_view, nullptr);
+}
+
+VkImageView ImageView::vk() const
+{
+    return m_image_view;
 }
--- src/vulkan/image_view.h
+++ src/vulkan/image_view.h
@@ -15,10 +15,7 @@ class ImageView
     const Image& m_image;
     const VkImageView m_image_view;
 
-    VkImageView create_image_view(VkImageAspectFlags aspect_mask, std::optional<uint32_t> base_layer);
-
   public:
-    // base_layer: if specified, creates a view for a single layer in a 2D array starting at base_layer;
     ImageView(
         const Device& device,
         const Image& image,
@@ -31,9 +28,6 @@ class ImageView
     ImageView(ImageView&&) = delete;
     ImageView& operator=(ImageView&&) = delete;
 
-    [[nodiscard]] VkImageView vk() const
-    {
-        return m_image_view;
-    }
+    [[nodiscard]] VkImageView vk() const;
 };
 } // namespace steeplejack
