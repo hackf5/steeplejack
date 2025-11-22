d7ad58b94b36153da2728d50f822e1c67e85a5d5
Brian Tyler <brian@hackf5.io>
2025-11-22 00:35:04 +0000

    refactor: move pipeline creation and descriptor fetching functions into a separate namespace

--- src/vulkan/graphics_pipeline.cpp
+++ src/vulkan/graphics_pipeline.cpp
@@ -6,48 +6,31 @@
 
 using namespace steeplejack;
 
-GraphicsPipeline::GraphicsPipeline(
+namespace
+{
+[[nodiscard]] VkPipeline create_pipeline(
     const Device& device,
-    DescriptorSetLayout& descriptor_set_layout,
+    VkPipelineLayout pipeline_layout,
     const RenderPass& render_pass,
     const std::string& vertex_shader,
-    const std::string& fragment_shader) :
-    m_device(device),
-    m_descriptor_set_layout(descriptor_set_layout),
-    m_pipeline_layout(m_descriptor_set_layout.create_pipeline_layout()),
-    m_pipeline(create_pipeline(render_pass, vertex_shader, fragment_shader)),
-    vkCmdPushDescriptorSetKHR(fetch_vkCmdPushDescriptorSetKHR())
-{
-}
-
-GraphicsPipeline::~GraphicsPipeline()
-{
-    spdlog::info("Destroying Graphics Pipeline");
-    vkDestroyPipeline(m_device.vk(), m_pipeline, nullptr);
-    vkDestroyPipelineLayout(m_device.vk(), m_pipeline_layout, nullptr);
-}
-
-VkPipeline GraphicsPipeline::create_pipeline(
-    const RenderPass& render_pass, const std::string& vertex_shader, const std::string& fragment_shader)
+    const std::string& fragment_shader)
 {
     spdlog::info("Creating Graphics Pipeline");
 
     auto vertex_input_state = VertexInputState::make_all(0);
 
-    auto vertex_shader_module = ShaderModule(m_device, vertex_shader);
-    auto fragment_shader_module = ShaderModule(m_device, fragment_shader);
+    auto vertex_shader_module = ShaderModule(device, vertex_shader);
+    auto fragment_shader_module = ShaderModule(device, fragment_shader);
     auto shader_stages = pipeline::create_shader_stages(vertex_shader_module, fragment_shader_module);
 
     auto input_assembly_state = pipeline::create_input_assembly_state();
-
     auto viewport_state = pipeline::create_dynamic_viewport_state();
-
     auto rasterization_state = pipeline::create_rasterization_state();
 
     VkPipelineMultisampleStateCreateInfo multisampling_state = {};
     multisampling_state.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
     multisampling_state.sampleShadingEnable = VK_FALSE;
-    multisampling_state.rasterizationSamples = m_device.msaa_samples();
+    multisampling_state.rasterizationSamples = device.msaa_samples();
 
     VkPipelineColorBlendAttachmentState color_blend_attachment = {};
     color_blend_attachment.blendEnable = VK_FALSE;
@@ -72,13 +55,13 @@ VkPipeline GraphicsPipeline::create_pipeline(
     pipeline_info.pColorBlendState = &color_blend_state;
     pipeline_info.pDynamicState = &dynamic_state;
     pipeline_info.pDepthStencilState = &depth_stencil_state;
-    pipeline_info.layout = m_pipeline_layout;
+    pipeline_info.layout = pipeline_layout;
     pipeline_info.renderPass = render_pass.vk();
     pipeline_info.subpass = 0;
     pipeline_info.basePipelineHandle = nullptr;
 
     VkPipeline pipeline = nullptr;
-    if (vkCreateGraphicsPipelines(m_device.vk(), nullptr, 1, &pipeline_info, nullptr, &pipeline) != VK_SUCCESS)
+    if (vkCreateGraphicsPipelines(device.vk(), nullptr, 1, &pipeline_info, nullptr, &pipeline) != VK_SUCCESS)
     {
         throw std::runtime_error("Failed to create pipeline");
     }
@@ -86,14 +69,64 @@ VkPipeline GraphicsPipeline::create_pipeline(
     return pipeline;
 }
 
-PFN_vkCmdPushDescriptorSetKHR GraphicsPipeline::fetch_vkCmdPushDescriptorSetKHR()
+[[nodiscard]] PFN_vkCmdPushDescriptorSetKHR
+fetch_vkCmdPushDescriptorSetKHR(const Device& device) // NOLINT(readability-identifier-naming)
 {
-    auto result = reinterpret_cast<PFN_vkCmdPushDescriptorSetKHR>(
-        vkGetDeviceProcAddr(m_device.vk(), "vkCmdPushDescriptorSetKHR"));
+    auto result =
+        reinterpret_cast<PFN_vkCmdPushDescriptorSetKHR>(vkGetDeviceProcAddr(device.vk(), "vkCmdPushDescriptorSetKHR"));
     if (result == nullptr)
     {
         throw std::runtime_error("Failed to load vkCmdPushDescriptorSetKHR");
     }
 
     return result;
+}
+} // namespace
+
+GraphicsPipeline::GraphicsPipeline(
+    const Device& device,
+    DescriptorSetLayout& descriptor_set_layout,
+    const RenderPass& render_pass,
+    const std::string& vertex_shader,
+    const std::string& fragment_shader) :
+    m_device(device),
+    m_descriptor_set_layout(descriptor_set_layout),
+    m_pipeline_layout(m_descriptor_set_layout.create_pipeline_layout()),
+    m_pipeline(create_pipeline(device, m_pipeline_layout, render_pass, vertex_shader, fragment_shader)),
+    vkCmdPushDescriptorSetKHR(fetch_vkCmdPushDescriptorSetKHR(device))
+{
+}
+
+GraphicsPipeline::~GraphicsPipeline()
+{
+    spdlog::info("Destroying Graphics Pipeline");
+    vkDestroyPipeline(m_device.vk(), m_pipeline, nullptr);
+    vkDestroyPipelineLayout(m_device.vk(), m_pipeline_layout, nullptr);
+}
+
+VkPipelineLayout GraphicsPipeline::layout() const
+{
+    return m_pipeline_layout;
+}
+
+DescriptorSetLayout& GraphicsPipeline::descriptor_set_layout() const
+{
+    return m_descriptor_set_layout;
+}
+
+void GraphicsPipeline::bind(VkCommandBuffer command_buffer) const
+{
+    vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
+}
+
+void GraphicsPipeline::push_descriptor_set(VkCommandBuffer command_buffer) const
+{
+    auto write_descriptor_sets = m_descriptor_set_layout.get_write_descriptor_sets();
+    vkCmdPushDescriptorSetKHR(
+        command_buffer,
+        VK_PIPELINE_BIND_POINT_GRAPHICS,
+        m_pipeline_layout,
+        0,
+        write_descriptor_sets.size(),
+        write_descriptor_sets.data());
 }
--- src/vulkan/graphics_pipeline.h
+++ src/vulkan/graphics_pipeline.h
@@ -20,11 +20,6 @@ class GraphicsPipeline
 
     PFN_vkCmdPushDescriptorSetKHR vkCmdPushDescriptorSetKHR; // NOLINT(readability-identifier-naming)
 
-    VkPipeline create_pipeline(
-        const RenderPass& render_pass, const std::string& vertex_shader, const std::string& fragment_shader);
-
-    PFN_vkCmdPushDescriptorSetKHR fetch_vkCmdPushDescriptorSetKHR(); // NOLINT(readability-identifier-naming)
-
   public:
     GraphicsPipeline(
         const Device& device,
@@ -39,31 +34,12 @@ class GraphicsPipeline
     GraphicsPipeline(GraphicsPipeline&&) = delete;
     GraphicsPipeline& operator=(GraphicsPipeline&&) = delete;
 
-    [[nodiscard]] VkPipelineLayout layout() const
-    {
-        return m_pipeline_layout;
-    }
+    [[nodiscard]] VkPipelineLayout layout() const;
 
-    [[nodiscard]] DescriptorSetLayout& descriptor_set_layout() const
-    {
-        return m_descriptor_set_layout;
-    }
+    [[nodiscard]] DescriptorSetLayout& descriptor_set_layout() const;
 
-    void bind(VkCommandBuffer command_buffer) const
-    {
-        vkCmdBindPipeline(command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
-    }
+    void bind(VkCommandBuffer command_buffer) const;
 
-    void push_descriptor_set(VkCommandBuffer command_buffer) const
-    {
-        auto write_descriptor_sets = m_descriptor_set_layout.get_write_descriptor_sets();
-        vkCmdPushDescriptorSetKHR(
-            command_buffer,
-            VK_PIPELINE_BIND_POINT_GRAPHICS,
-            m_pipeline_layout,
-            0,
-            write_descriptor_sets.size(),
-            write_descriptor_sets.data());
-    }
+    void push_descriptor_set(VkCommandBuffer command_buffer) const;
 };
 } // namespace steeplejack
